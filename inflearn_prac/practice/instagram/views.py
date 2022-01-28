67bf742bc574434e302f37c95b1bfc4c6f39d60e
Minwook11 <alsdnr4874@gmail.com>
2022-01-28 17:24:44 +0900

    Queryset과 Template을 활용한 간단한 검색 구현해보기 - 강의 항목 중 "모델을 통한 조회", "Qeuryset을 통한 간단 검색 구현" - DB에 저장된 정보를 조회하고 간단한 검색을 구현하해보기 위한 실습 	- DB에 주어진 값으로 조회를 진행할 post_list Method 구현 	- 간단한 Template 구현 및 For, If 문법 사용 -- 이미지 링크 여부에 따른 작동 	- Template과 View 사이의 데이터 IO를 확인 -- 검색어 보여주기 작업

--- inflearn_prac/practice/instagram/views.py
+++ inflearn_prac/practice/instagram/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+def post_list(request):
+	query_set = Post.objects.all()
+	q = request.GET.get('q', '')	# 입력받은 검색 조건이 있으면 q에 저장 없으면 ''
+	if q:
+		query_set = query_set.filter(message__icontains = q)	# 검색어가 포함된 Queryset만 필터링
+	return render(request, 'instagram/post_list.html', {
+		'post_list' : query_set,	# 결과 Queryset 전달
+		'q' : q						# 입력받은 검색 조건 전달
+	})
