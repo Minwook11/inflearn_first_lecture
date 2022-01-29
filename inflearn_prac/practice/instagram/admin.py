30fe2c6d34f17b14002f6491c1f3d0b894e8e0a6
Minwook11 <alsdnr4874@gmail.com>
2022-01-29 16:27:14 +0900

    Foreign Key 필드를 사용하여 관계를 구현 - "관계를 표현하는 모델 필드(ForeignKey) - 1:N관계에 해당되는 모델을 추가로 정의하고 ForeignKey 필드에 대한 기초 실습 	- ForeignKey 필드의 인자 중 to, on_delete 는 필수로 지정해야 함 	- reverse_name이라는 개념을 통해서 1에 해당하는 객체에서 모든 N 객체 접근 가능 	- related_name이라는 옵션을 사용해서 reverse_name 지정 가능 	- Django의 기본 User모델을 사용하는 올바른 방법 실습

--- inflearn_prac/practice/instagram/admin.py
+++ inflearn_prac/practice/instagram/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Post
+from .models import Post, Comment
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 	# Admin 페이지에서 보여줄 Models 객체들의 데이터 지정
 	# Models 클래스에 정의된 Method도 지정할 수 있다. ex) --> message_length
-	list_display = ['id', 'message', 'image_thumb', 'is_public', 'message_length', 'created_at', 'updated_at']
+	list_display = ['id', 'author', 'message', 'image_thumb', 'is_public', 'message_length', 'created_at', 'updated_at']
 	list_display_links = ['id', 'message'] # Admin 페이지에서 링크 활성화 할 Row 지정
 	# Admin에서 제공하는 검색 UI를 활용하여, 검색할 수 있는 대상을 지정
 	search_fields = ['message']
@@ -23,3 +23,7 @@ class PostAdmin(admin.ModelAdmin):
 		if post.image:
 			return mark_safe(f'<img src="{post.image.url}" style="width: 50px" />') # 이미지 태그를 사용하기위한 mark_safe() 메소드 활용 및 스타일 태그 바로 적용 가능
 		return None
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ['id', 'post', 'message', 'created_at', 'updated_at']
--- inflearn_prac/practice/instagram/models.py
+++ inflearn_prac/practice/instagram/models.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.db import models
 
 class Post(models.Model):
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	message = models.TextField()
 	# ImageField는 pillow 라이브러리 dependency 존재, upload_to 옵션을 통해서 settings의 MEDIA_ROOT와 조합하여 파일이 저장될 위치를 지정 가능함
 	# upload_to에서 시간관련 키워드를 사용해서 원하는 시간 단위로 저장 파일을 구분할 수 있다.
@@ -20,3 +22,13 @@ class Post(models.Model):
 	def message_length(self):
 		return len(self.message)
 	message_length.short_description = "메세지 글자수" # Admin 페이지에서의 Row 이름 지정, Default는 함수 이름
+
+class Comment(models.Model):
+	# limit_choices_to 옵션을 사용해서 조건을 지정할 수 있음
+	# related_name 옵션을 통해서 reverse_name을 지정할 수 있음, 기본은 소문자 모델 클래스 이름 + '_set'
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, 
+			limit_choices_to={'is_public':True},	
+			related_name='current_post_comment')
+	message = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
