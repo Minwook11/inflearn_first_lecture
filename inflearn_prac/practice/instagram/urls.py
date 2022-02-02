06d8e43c276115dd288ef28901b4ccb55a8f5076
Minwook11 <alsdnr4874@gmail.com>
2022-02-02 21:13:09 +0900

    Views의 특성을 알 수 있는 여러 응답의 FBV - "다양한 응답의 함수 기반 뷰" 강의 - Django Views의 가장 중요한 특징인 HttpResponse 객체의 필수 리턴 	- HttpResponse 객체는 File like 객체 또는 문자열/바이너리 응답 지원 	- 위의 특성으로 인해서 Spread Sheet(Pandas), Image(Pillow)등 사용 가능 - Views와 연결하는 urls에 정규 표현식 적용 가능 -- re_path - FBV가 아닌 CBV에 대해서 간략하게 실습-- Django 기본 제공 ListView - Views에서 URL Captured Values를 사용한 간단한 Views 실습-- 연결만 확인

--- inflearn_prac/practice/instagram/urls.py
+++ inflearn_prac/practice/instagram/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .import views
 
 urlpatterns = [
 	path('', views.post_list),
+	path('<int:id>/', views.post_detail),
+#	re_path(r'(?P<id>\d+)/$', views.post_detail),	# 정규 표현식 사용례, 위와 동일하게 작동
 ]
--- inflearn_prac/practice/instagram/views.py
+++ inflearn_prac/practice/instagram/views.py
@@ -1,13 +1,24 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Post
 
-def post_list(request):
-	query_set = Post.objects.all()
-	q = request.GET.get('q', '')	# 입력받은 검색 조건이 있으면 q에 저장 없으면 ''
-	if q:
-		query_set = query_set.filter(message__icontains = q)	# 검색어가 포함된 Queryset만 필터링
-	return render(request, 'instagram/post_list.html', {
-		'post_list' : query_set,	# 결과 Queryset 전달
-		'q' : q						# 입력받은 검색 조건 전달
-	})
+post_list = ListView.as_view(model = Post)	# Django에서 기본 제공하는 ListView, 아래 Views와 차이점은 검색이 안됨
+
+#def post_list(request):
+#	query_set = Post.objects.all()
+#	q = request.GET.get('q', '')	# 입력받은 검색 조건이 있으면 q에 저장 없으면 ''
+#	if q:
+#		query_set = query_set.filter(message__icontains = q)	# 검색어가 포함된 Queryset만 필터링
+#	return render(request, 'instagram/post_list.html', {
+#		'post_list' : query_set,	# 결과 Queryset 전달
+#		'q' : q						# 입력받은 검색 조건 전달
+#	})
+
+# Views의 인자 중 URL에서 읽어오는 인자를 URL Captured Values라고 말함
+def post_detail(request, id):
+	response = HttpResponse()
+	response.write('Hello World')
+
+	return response
