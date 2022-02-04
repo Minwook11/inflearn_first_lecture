b89c47eec6372f5c82d5edb428150c82fe8f8ac0
Minwook11 <alsdnr4874@gmail.com>
2022-02-04 21:48:30 +0900

    오타 수정

--- inflearn_prac/practice/instagram/admin.py
+++ inflearn_prac/practice/instagram/admin.py
@@ -11,7 +11,7 @@ class PostAdmin(admin.ModelAdmin):
 	list_display_links = ['id', 'message'] # Admin 페이지에서 링크 활성화 할 Row 지정
 	# Admin에서 제공하는 검색 UI를 활용하여, 검색할 수 있는 대상을 지정
 	search_fields = ['message']
-	# Admin에서 제공하는 필터 UI를 활ㅇ용하여, 필터링의 기준이 될 대상을 지정
+	# Admin에서 제공하는 필터 UI를 활용하여, 필터링의 기준이 될 대상을 지정
 	list_filter = ['created_at', 'is_public']
 
 	# Method는 Admin 에서도 선언하여 사용 가능하다. 목적이나 빈도에 따라서 Models에 선언할 지, Admin에 선언할 지 선택하여 사용
