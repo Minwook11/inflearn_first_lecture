e52e35f3034df4db961beae60d9080c0e5cad2d1
Minwook11 <alsdnr4874@gmail.com>
2022-02-08 16:46:39 +0900

    Django의 Static 파일 관리 - "장고가 static파일을 다루는 방법" 강의 - Django에서 Static 파일을 다루기 위한 방법과 동작 방식 	- 프로젝트 settings 파일에 static 파일을 위한 설정을 추가 	- colletcstatic 명령어를 수행해보았다. 	- 일단 static 파일은 git에서 무시하도록 설정 -- 이후 변경 가능성 높음 - Bootstrap, Jquery 파일을 직접 가지고 사용하는 방법으로 instagram 앱의 layout 설정

--- inflearn_prac/practice/practice/settings.py
+++ inflearn_prac/practice/practice/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, 'practice', 'static'),
+]
 
 # Media files
 MEDIA_URL = '/media/'	# 미디어 파일에 접근할 때 활용
