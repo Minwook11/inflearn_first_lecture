0fda63ed7371fd4001ba6c32e3bd51a9c0f03080
Minwook11 <alsdnr4874@gmail.com>
2022-02-09 20:11:04 +0900

    Django의 Form에서 진행되는 Validation들 - "Form Validation" 강의 - Django Form에서 사용할 수 있는 여러 Vaildation에 대해서 알아보고 실습 진행 	- 크게 clean_필드명 Method, validator로 나뉨. 	- validator의 경우 기본적으로 bulit-in되어 있는 클래스들을 사용하는 것이 주 		- 당연히, 목적에 맞게 이를 상속하여 커스텀할 수도 있음 		- validator는 Model Layer에 사용하는 것이 좋다. 	- clean Method의 경우, 원하는 목적, 형태에 맞게 구현하여 사용하는 것이 주 		- clean Method는 validator와 가장 큰 차이점이 있는데 값을 변경할 수 있음 		- forms.py에 예제 진행함 	- clean Method 및 validator의 특징을 잘 파악하고 상황에 맞게 사용할 수 있어야 함

--- inflearn_prac/practice/instagram/forms.py
+++ inflearn_prac/practice/instagram/forms.py
@@ -5,3 +5,10 @@ class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ['message', 'image', 'tag_set', 'is_public']
+
+	# clean_필드명 메소드를 사용하는 예제
+	def clean_message(self):
+		message = self.cleaned_data.get('message')
+		if message:
+			message = re.sub(r'[a-zA-z]+', '', message)	#입력값의 변환 정규식 -- 영어만 기록
+		return message
