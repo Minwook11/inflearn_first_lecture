bf3b94a4d0af9969cfe40bfa329e12e51c925384
Minwook11 <alsdnr4874@gmail.com>
2022-02-11 21:17:38 +0900

    기본 제공되는 CBV를 사용해서 지금까지 구현한 기능 재구현 - "장고 기본 CBV API(Generic editing views)" 강의 - 지금까지 FBV를 사용해서 구현한 포스팅 작성, 수정, 삭제 기능을 CBV를 사용해서 구현 	- 새글 작성 -- CreateView를 사용하여 작성 	- 글 수정 -- UpdateView를 사용하여 수정 	- 글 삭제 -- DeleteView를 사용해서 삭제 	- 기존 FBV와 비교하면 코드도 짧아지고 사용도 편리함. 그래도 FBV방식은 알아둘 것 	- CBV 기반으로 수정을 하다보니, urlConf쪽에서 id에서 pk로 키 네임 변경이 필요 		- 이 부분은 따로 지정해 줄 수 있을 것 같다.

--- inflearn_prac/practice/instagram/urls.py
+++ inflearn_prac/practice/instagram/urls.py
@@ -15,8 +15,8 @@ urlpatterns = [
 #	Reverse Name 지정 후 RedirectView를 통해서 테스트 진행
 	path('', views.post_list, name='post_list'),
 	path('<int:id>/', views.post_detail, name='post_detail'),
-	path('<int:id>/edit/', views.post_edit, name='post_edit'),
-	path('<int:id>/delete/', views.post_delete, name='post_delete'),
+	path('<int:pk>/edit/', views.post_edit, name='post_edit'),
+	path('<int:pk>/delete/', views.post_delete, name='post_delete'),
 #	re_path(r'(?P<id>\d+)/$', views.post_detail),	# 정규 표현식 사용례, 위와 동일하게 작동
 #	path('archives/<int:year>/', views.archives_year),
 #	re_path(r'archives/(?P<year>\d+)/', views.archives_year),
--- inflearn_prac/practice/instagram/views.py
+++ inflearn_prac/practice/instagram/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView
+from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
@@ -10,61 +11,100 @@ from .models import Post
 from .forms import PostForm
 
 
-# 새로운 포스팅 작성 기능을 Model Form을 사용해서 구현
-@login_required
-def post_new(request):
-	if request.method == 'POST':
-		form = PostForm(request.POST, request.FILES)
-		if form.is_valid():
-			# 현재 Form에서 author 필드를 다루지 않기 때문에 commit옵션 False를 활용
-			post = form.save(commit=False)
-			post.author = request.user
-			post.save()
-			messages.success(request, 'Posting successfully saved')
-			return redirect(post)
-	else:
-		form = PostForm()
+# 새로운 포스팅 작성 기능을 Model Form을 사용해서 구현 -- FBV 사용
+#@login_required
+#def post_new(request):
+#	if request.method == 'POST':
+#		form = PostForm(request.POST, request.FILES)
+#		if form.is_valid():
+#			# 현재 Form에서 author 필드를 다루지 않기 때문에 commit옵션 False를 활용
+#			post = form.save(commit=False)
+#			post.author = request.user
+#			post.save()
+#			messages.success(request, 'Posting successfully saved')
+#			return redirect(post)
+#	else:
+#		form = PostForm()
+#
+#	return render(request, 'instagram/post_form.html', {
+#			'form' : form,
+#			'post' : None
+#		})
 
-	return render(request, 'instagram/post_form.html', {
-			'form' : form,
-			'post' : None
-		})
+# 기본 제공되는 CreateView를 사용해서 새로운 포슽팅 작성 기능을 구현 -- CBV 사용
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = Post
+	form_class = PostForm
 
-# 포스팅 수정 기능을 Model Form을 사용해서 구현, 로그인 필요는 장식자 사용으로 적용
-@login_required
-def post_edit(request, id):
-	post = get_object_or_404(Post, id = id)
+	def form_valid(self, form):
+		self.object = form.save(commit=False)
+		self.object.author = self.request.user
+		messages.success(self.request, 'Posting successfully saved')
+		return super().form_valid(form)
+
+post_new = PostCreateView.as_view()
+
+# 포스팅 수정 기능을 Model Form을 사용해서 구현, 로그인 필요는 장식자 사용으로 적용 -- FBV 사용
+#@login_required
+#def post_edit(request, id):
+#	post = get_object_or_404(Post, id = id)
+#	
+## 작성자 Check 로직 --> 이를 장식자화 해보는 것도 좋은 방법, 다른 경우도 
+#	if post.author != request.user:
+#		messages.error(request, 'Only Author can edit')
+#		return redirect(post)
+#
+#	if request.method == 'POST':
+#		form = PostForm(request.POST, request.FILES)
+#		if form.is_valid():
+#			post = form.save()
+#			messages.success(request, 'Posting successfully editted')
+#			return redirect(post)
+#	else:
+#		form = PostForm(instance = post)
+#
+#	return render(request, 'instagram/post_form.html', {
+#			'form' : form,
+#			'post' : post,
+#		})
+
+# 기본 제공되는 UpdateView를 사용해서 포스팅 수정 기능을 구현 -- CBV 사용
+class PostUpdateView(UpdateView):
+	model = Post
+	form_class = PostForm
+
+	def form_valid(self, form):
+		messages.success(self.request, ' Posting successfully editted')
+		return super().form_valid(form)
+
+post_edit = PostUpdateView.as_view()
+
+# 삭제 기능을 구현 -- FBV 사용
+#@login_required
+#def post_delete(request, id):
+#	post = get_object_or_404(Post, id = id)
+#	if request.method == 'POST':
+#		post.delete()
+#		messages.success(request, 'Posting was successfully deleted')
+#		return redirect('instagram:post_list')
+#	return render(request, 'instagram/post_confirm_delete.html', {
+#			'post' : post
+#		})
+
+class PostDeleteView(DeleteView):
+	model = Post
+	# 삭제 이후 연결 URL을 간단하게 지정해주는 방법
+#	success_url = '/instagram/'
 	
-# 작성자 Check 로직 --> 이를 장식자화 해보는 것도 좋은 방법, 다른 경우도 
-	if post.author != request.user:
-		messages.error(request, 'Only Author can edit')
-		return redirect(post)
+	# reverse_lazy를 사용해서 삭제 이후 URL 지정, __lazy가 붙으면 해당 값이 필요할 때에 수행해서 값을 가져옴
+	# 기본적으로 Django가 초기화 되는 시점과 해당 데이터가 로드되는 시점이 달라서 생기는 문제
+	success_url = reverse_lazy('instagram:post_list')
 
-	if request.method == 'POST':
-		form = PostForm(request.POST, request.FILES)
-		if form.is_valid():
-			post = form.save()
-			messages.success(request, 'Posting successfully editted')
-			return redirect(post)
-	else:
-		form = PostForm(instance = post)
+	# __lazy를 사용하고 싶지 않다면 아래와 같은 형태로 오버라이드가 필요하다.
+#	def get_success_url(self):
+#		return reverse('instagram:post_liat')
 
-	return render(request, 'instagram/post_form.html', {
-			'form' : form,
-			'post' : post,
-		})
-
-# 삭제 기능을 구현
-@login_required
-def post_delete(request, id):
-	post = get_object_or_404(Post, id = id)
-	if request.method == 'POST':
-		post.delete()
-		messages.success(request, 'Posting was successfully deleted')
-		return redirect('instagram:post_list')
-	return render(request, 'instagram/post_confirm_delete.html', {
-			'post' : post
-		})
+post_delete = PostDeleteView.as_view()
 
 # Django에서 기본 제공하는 ListView, 아래 Views와 차이점은 검색 안됨
 # paginate_by 옵션을 통해서 기본 지원되는 페이징 기능 실습
