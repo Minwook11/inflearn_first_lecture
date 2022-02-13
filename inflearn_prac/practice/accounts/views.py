f205de3cc3349b024e368980649f00e69bfa2fc6
Minwook11 <alsdnr4874@gmail.com>
2022-02-13 23:05:15 +0900

    사용자 프로필 페이지 및 수정 기능 구현 - "사용자 프로필 페이지 및 프로필 수정" 강의 - 로그인 성공의 경우, 리다이렉트를 위한 사용자 프로필 페이지 구현 	- Profile 모델에 정의한 간단한 필드값 표시 	- 표시된 필드 값을 수정할 수 있도록 수정 기능 적용 	- 기존 값이 존재할 때에는 기존 값이 수정 페이지에서 보이도록 설정

--- inflearn_prac/practice/accounts/views.py
+++ inflearn_prac/practice/accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import UpdateView
 
-# Create your views here.
+from .forms import ProfileForm
+from .models import Profile
+
+@login_required
+def profile(request):
+	return render(request, 'accounts/profile_form.html')
+
+#class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+#	model = Profile
+#	form_class = ProfileForm
+#
+#profile_edit = ProfileUpdateView.as_view()
+
+@login_required
+def profile_edit(request):
+	try:
+		profile = request.user.profile
+	except Profile.DoesNotExist:
+		profile = None
+
+	if request.method == 'POST':
+		form = ProfileForm(request.POST, request.FILES, instance=profile)
+		if form.is_valid:
+			form.save(commit=False)
+			profile.user = request.user
+			profile.save()
+			return redirect('profile')
+	else:
+		form = ProfileForm(instance=profile)
+	return render(request, 'accounts/profile_edit.html',
+		{'form' : form
+	})
+
