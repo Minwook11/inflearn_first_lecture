6f947d593115ca72dcc8b226ccf6bcea821f1517
Minwook11 <alsdnr4874@gmail.com>
2022-02-15 15:33:22 +0900

    기본 로그인 기능 활용 - "회원가입하자마자 로그인하기" 강의 - Django에서 제공되는 로그인 모듈을 활용하여 회원가입 성공 시 로그인 한 상태로 연결 	- auth의 login 모듈을 사용하여 회원가입 성공 시 자동으로 로그인 되도록 설정 	- 지속적인 지원이 이루어지기 때문에 기본 모듈을 충분히 활용하는 게 좋다.

--- inflearn_prac/practice/accounts/views.py
+++ inflearn_prac/practice/accounts/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
@@ -41,13 +41,19 @@ def profile_edit(request):
 		{'form' : form
 	})
 
-signup = CreateView.as_view(
-		model = User,
-		form_class = UserCreationForm,
-		success_url = settings.LOGIN_URL,
-		template_name = 'accounts/signup_form.html',
-		)
+class SignupView(CreateView):
+	model = User
+	form_class = UserCreationForm
+	success_url = settings.LOGIN_REDIRECT_URL
+	template_name = 'accounts/signup_form.html'
 
+	def form_valid(self, form):
+		response = super().form_valid(form)
+		user = self.object
+		auth_login(self.request, user)
+		return response
+
+signup = SignupView.as_view()
 
 def logout(request):
 	pass
