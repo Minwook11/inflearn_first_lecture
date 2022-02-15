3bfd9fb9900516d9d01f0c5564f4063d90d83be6
Minwook11 <alsdnr4874@gmail.com>
2022-02-15 20:55:23 +0900

    DRF의 샘플코드를 실습 - "JSON 응답뷰 만들기" 강의 - DRF의 샘플코드를 실습하면서 DRF의 중요한 요소들을 확인 	- CBV 기반의 ViewSet, ModelViewSet을 통해 간단한 View 작성 	- ModleSerializer를 사용하여 간단한 Serializer를 작성 	- DefaultRouter를 사용하여 기본적인 API 라우팅 설정

--- inflearn_drf/drf_practice/instagram/views.py
+++ inflearn_drf/drf_practice/instagram/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+class PostViewSet(ModelViewSet):
+	queryset = Post.objects.all()
+	serializer_class = PostSerializer
+
+	# Client에서 보내는 reqeust의 인코딩에 따라서 구성이 달라지는 것을 확인할 수 있는 dispatch 메소드 오버라이딩
+#	def dispatch(self, request, *args, **kwargs):
+#		print("reqeust.body : ", request.body)
+#		print("reqeust.POST : ", request.POST)
+#		return super().dispatch(request, *args, **kwargs)
